46eded1f6e7f9d2dde629b045947c5e9520b234e
sowjanya25071993 <136591283+sowjanya25071993@users.noreply.github.com>
2025-06-28 19:17:32 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull jujjurisowjanya/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000  jujjurisowjanya/simple-python-flask-app
+docker run -d -p 8000:8000  jujjurisowjanya/simple-python-flask-app
